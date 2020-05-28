9ecdbcdb15cceba80e483e9c7b64d7203680db43
mai1121 <maimaiukawa@gmail.com>
2020-05-29 06:36:38 +0900

    first commit

---
+++ Books_Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Books_Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
